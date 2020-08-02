fb8f1c706de3b461cc91bb90221e954e5894a360
JerryNYang <46272314+JerryNYang@users.noreply.github.com>
2020-08-02 11:01:26 -0500

    Initial Committ
    This a simple Kotlin Application (1st Edition)

---
+++ app/src/main/java/com/inyang/jerry/appone/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.inyang.jerry.appone
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
